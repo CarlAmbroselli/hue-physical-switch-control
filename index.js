40d14ce941e02bdc8d1df99db722c95c90b9614e
Carl Ambroselli <git@carl-ambroselli.de>
2023-02-23 16:32:35 +0000

    switch to axios

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const YAML = require('yaml');
 const http = require('http');
-const fetch = require('node-fetch');
+import axios from 'axios';
 const url = require('url');
 const file = fs.readFileSync('./config.yaml', 'utf8')
 config = YAML.parse(file)
@@ -9,8 +9,7 @@ config = YAML.parse(file)
 let lampKey = '-1';
 
 function init() {
-  fetch(`http://${config.gatewayIp}/api/${config.token}/lights`)
-  .then(res => res.json())
+  axios.get(`http://${config.gatewayIp}/api/${config.token}/lights`)
   .then(hueStatus => {
     lampKey = Object.keys(hueStatus).find(key => hueStatus[key].name === config.hueLightName);
     console.log("Ready!");
@@ -20,17 +19,13 @@ function init() {
 init();
 
 async function handle(switchState) {
-  fetch(`http://${config.gatewayIp}/api/${config.token}/lights/${lampKey}`)
-    .then(res => res.json())
+  axios.get(`http://${config.gatewayIp}/api/${config.token}/lights/${lampKey}`)
     .then(hueStatus => {
       console.log(lampKey, hueStatus)
       const lampState = hueStatus.state.on;
-      fetch(`http://${config.gatewayIp}/api/${config.token}/lights/${lampKey}/state`, {
-          "method": "PUT",
-          "headers": {
-                "Content-Type": "application/json; charset=utf-8"
-          },
-          "body": `{"on":${!lampState}, "bri": 255}`
+      axios.put(`http://${config.gatewayIp}/api/${config.token}/lights/${lampKey}/state`, {
+        "on": !lampState,
+        "bri": 255
       }).then(() => { console.log("toggled lamp state to " + !lampState + new Date()); });
     }); 
 }
